43abd4e0ad979a31fb21f7c44c405d0b8d26a733
Arnav Singh <arnavsengar1506@gmail.com>
2026-06-22 20:41:10 +0530

    Mon Jun 22 08:41:10 PM IST 2026

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ const transactionRoutes = require('./routes/transaction.routes')
 app.use(express.json())
 app.use(cookieParser())
 
-
+app.use("/",(req,res)=>{
+    res.send("Ledger service is up and running")
+})
 app.use("/api/auth",authRouter)
 app.use("/api/accounts",accountRouter)
 app.use("/api/transactions",transactionRoutes)
